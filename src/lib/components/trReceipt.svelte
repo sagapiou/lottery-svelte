d6193d43206db0443fb71146f4277f3b8a25564e
sagapiou <sagapiou@gmail.com>
2022-06-09 21:21:58 +0800

    styling

---
+++ src/lib/components/trReceipt.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let transReceipt
+</script>
+
+{#if transReceipt !== null}
+    <div>
+        {#if transReceipt.status == 1}
+            <h1>Transaction Success</h1>
+        {:else}
+            <h1>Transaction failed</h1>
+        {/if}
+        <h2>Address From: {transReceipt.from}</h2>
+        <h2>Address To: {transReceipt.to}</h2>
+        <h3>Transaction Block number: {transReceipt.blockNumber}</h3>
+        <h3>Transaction index in block: {transReceipt.transactionIndex}</h3>
+        <h3>Confirmations: {transReceipt.confirmations}</h3>
+        <h3>Gas used: {transReceipt.gasUsed}</h3>
+        <h3>Gas price: {transReceipt.effectiveGasPrice}</h3>
+    </div>
+{/if}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,6 +13,7 @@
     import { blockNr, blockData, blockDatawithTransactions } from "$lib/utils/ethersBlocks"
     import lottery from "$lib/contracts/Lottery"
     import { connectToContract, connectSignerToContract } from "$lib/utils/ethersContracts"
+    import TrReceipt from "$lib/components/trReceipt.svelte"
 
     let balance
     let code, lotteryCode
@@ -23,6 +24,8 @@
     let { abi, contractName } = lottery
     let contractLottery
     let sendPlayerContract = false
+    let playerContractReply = false
+    let transactionReceipt = null
 
     onMount(async () => {
         await connectWallet()
@@ -93,10 +96,15 @@
         let tx = await contractLottery.enterLottery({
             value: ethers.utils.parseUnits("0.1", "ether"),
         })
-        let transactionReceipt = await tx.wait(1)
+        transactionReceipt = await tx.wait(1)
+        playerContractReply = true
         console.log("transactionReceipt: ", transactionReceipt)
         lotteryGetData()
         sendPlayerContract = false
+    }
+
+    const toggleMessage = () => {
+        playerContractReply = false
     }
 </script>
 
@@ -112,74 +120,86 @@
 </header>
 
 <main>
-    <div>
-        Lottery details : <br />
-        lotteryFee : {lotteryFee} ETH<br />
-        totalPlayers : {totalPlayers}<br />
-        recentWinner : {recentWinner}<br />
-        lastTimeStamp : {lastTimeStamp}<br />
-        totalTimeFromLastWinner : {blockTS - lastTimeStamp}<br />
-        players : {players}
-        {#each players as player}
-            <li>{player}</li>
-        {/each}
-        <br />
-        <br />
+    <div class="w-full">
+        <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+        <div class="modal modal-bottom sm:modal-middle">
+            <div class="modal-box">
+                <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                <p class="py-4">
+                    <TrReceipt transReceipt={transactionReceipt} />
+                </p>
+                <div class="modal-action">
+                    <label for="my-modal-6" class="btn" on:click={toggleMessage}>Yay!</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex ...">
+            <div class="w-1/4 ... place-content-center" />
+            <div class="w-1/4 ... place-content-center">
+                <div>
+                    <h1>Lottery details :</h1>
+                    <br />
+                    <hr />
+                    <br />
+                    <h2>lotteryFee : {lotteryFee} ETH</h2>
+                    <br />
+                    <h2>totalPlayers : {totalPlayers}</h2>
+                    <br />
+                    <h2>recentWinner : {recentWinner}</h2>
+                    <br />
+                    <h2>lastTimeStamp : {lastTimeStamp}</h2>
+                    <br />
+                    <h2>totalTimeFromLastWinner : {blockTS - lastTimeStamp}</h2>
+                    <br />
+                    <h2>players : {players}</h2>
+                    {#each players as player}
+                        <li>{player}</li>
+                    {/each}
+                </div>
+            </div>
+            <div class="w-1/4 ... place-content-center">
+                <tr class="center">
+                    {#if sendPlayerContract}
+                        <button class="btn loading" disabled>Lottery</button>
+                    {:else}
+                        <button class="btn" on:click={enterLottery}>Enter Lottery</button>
+                    {/if}
+                </tr>
+                <tr>
+                    <br />
+                    <hr />
+                    <br />
+                </tr>
+                <tr>
+                    {#if playerContractReply}
+                        <div class="alert shadow-lg">
+                            <div>
+                                <svg
+                                    xmlns="www.saga.net"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    class="stroke-info flex-shrink-0 w-6 h-6"
+                                    ><path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    /></svg
+                                >
+                                <div>
+                                    <h3 class="font-bold">New message!</h3>
+                                    <div class="text-xs">You have 1 unread message</div>
+                                </div>
+                            </div>
+                            <div class="flex-none">
+                                <label for="my-modal-6" class="btn modal-button">see</label>
+                            </div>
+                        </div>
+                    {/if}
+                </tr>
+            </div>
+            <div class="w-1/4 ... place-content-center" />
+        </div>
     </div>
-    {#if sendPlayerContract}
-        <button class="btn loading" disabled>Lottery</button>
-    {:else}
-        <button class="btn" on:click={enterLottery}>Enter Lottery</button>
-    {/if}
 </main>
-
-<footer>
-    <p>Brought to you by ...</p>
-</footer>
-
-<style>
-    main {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        padding: 1rem;
-        width: 100%;
-        max-width: 1024px;
-        margin: 0 auto;
-        box-sizing: border-box;
-    }
-
-    header {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 40px;
-    }
-
-    footer {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 40px;
-    }
-
-    footer a {
-        font-weight: bold;
-    }
-
-    @media (min-width: 480px) {
-        footer {
-            padding: 40px 0;
-        }
-    }
-
-    button {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 10em;
-    }
-</style>
