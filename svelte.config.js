ee12999f58505bd618438c5ea4e7577bb85cf5b3
sagapiou <sagapiou@gmail.com>
2022-06-10 22:58:26 +0800

    ipfs

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 //import adapter from "@sveltejs/adapter-auto"
-import adapter_static from "@sveltejs/adapter-static"
+import adapter from "@sveltejs/adapter-static"
 import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter_static(),
+        adapter: adapter(),
+        prerender: {
+            // This can be false if you're using a fallback (i.e. SPA mode)
+            default: true,
+        },
     },
-
     preprocess: preprocess(),
 }
 
