fa7e1ee9ae96bf47dfc88973b4a5098d762ce046
sagapiou <sagapiou@gmail.com>
2022-06-10 19:10:30 +0800

    static-adapter for ipfs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script>
     //import ConnectButton from "./connectButton.svelte"
-    
 </script>
 
 <svelte:head>
@@ -9,37 +8,5 @@
 </svelte:head>
 
 <section>
-    <!-- <ConnectButton /> -->
-
-    Lottery Details : <br>
-    <!-- <Wallet /> -->
+    Lottery Details : <br />
 </section>
-
-<style>
-    section {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        flex: 1;
-    }
-
-    h1 {
-        width: 100%;
-    }
-
-    .welcome {
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding: 0 0 calc(100% * 495 / 2048) 0;
-    }
-
-    .welcome img {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        display: block;
-    }
-</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from "@sveltejs/adapter-auto"
+//import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-static"
 import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            // default options are shown
+            pages: "build",
+            assets: "build",
+            fallback: null,
+            precompress: false,
+        }),
     },
     preprocess: [
         preprocess({
